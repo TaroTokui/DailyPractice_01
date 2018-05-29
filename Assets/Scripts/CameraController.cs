592c6ff0cea902165317000b1db66d77294b61d1
tokui <tokui@1-10.com>
2018-05-29 22:38:58 +0900

    change camera control parameters

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,16 +8,16 @@ public class CameraController : MonoBehaviour {
     public Transform targetObject;
 
     [SerializeField]
-    [Range(-10, 10)]
-    public float RotationSpeed = 0;
+    [Range(-50, 50)]
+    public float RotationSpeed = 10;
 
     [SerializeField]
     [Range(0, 50)]
     public float Distance = 10;
 
     [SerializeField]
-    [Range(0, 50)]
-    public float Hight = 10;
+    [Range(0, 90)]
+    public float HeightAngle = 10;
 
     // private variables
     private Vector3 mPosition;
@@ -35,14 +35,16 @@ public class CameraController : MonoBehaviour {
 	void Update () {
         //Vector3 pos = new Vector3(0,0,0);
 
-        mAngle = (Time.time * 0.1f % 360) * 2.0f * Mathf.PI * RotationSpeed;
+        mAngle = Mathf.Deg2Rad * (Time.time % 360) * RotationSpeed;
 
-        float x = Mathf.Cos(mAngle) * Distance;
-        float z = Mathf.Sin(mAngle) * Distance;
+        float h = Mathf.Sin(HeightAngle * Mathf.Deg2Rad) * Distance;
+        float w = Mathf.Sin(HeightAngle * Mathf.Deg2Rad) * Distance;
 
-        //mPosition = new Vector3(x, 20, z);
+        float x = Mathf.Cos(mAngle) * w;
+        float z = Mathf.Sin(mAngle) * w;
+        
         mPosition.x = x;
-        mPosition.y = Hight;
+        mPosition.y = h;
         mPosition.z = z;
         //this.transform.Rotate(0, (Input.GetAxis("Horizontal") * 1), 0);
         //this.transform.Translate(mPosition);
