8164dcaaca30493125783c8a4b3cd917797f54fb
tokui <tokui@1-10.com>
2018-05-30 08:00:25 +0900

    add osc receiver

---
+++ Assets/Scripts/OscReceiver.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace uOSC
+{
+    [RequireComponent(typeof(uOscServer))]
+    public class OscReceiver : MonoBehaviour
+    {
+        //[SerializeField]
+        public float _InputLow;
+
+        //[SerializeField]
+        public float _InputMid;
+
+        //[SerializeField]
+        public float _InputHi;
+
+        // Use this for initialization
+        void Start()
+        {
+            var server = GetComponent<uOscServer>();
+            server.onDataReceived.AddListener(OnDataReceived);
+        }
+
+        void OnDataReceived(Message message)
+        {
+            // address
+            var msg = message.address;
+            //Debug.Log(msg);
+            switch (msg)
+            {
+                case "/Low":
+                    //Debug.Log("low");
+                    _InputLow = (float)message.values[0];
+                    break;
+                case "/Mid":
+                    //Debug.Log("mid");
+                    _InputMid = (float)message.values[0];
+                    break;
+                case "/Hi":
+                    _InputHi = (float)message.values[0];
+                    break;
+                default:
+                    //Debug.Log("Incorrect intelligence level.");
+                    break;
+            }
+
+            //var msg = message.address + ": ";
+
+            //// timestamp
+            //msg += "(" + message.timestamp.ToLocalTime() + ") ";
+
+            //// values
+            //foreach (var value in message.values)
+            //{
+            //    msg += value.GetString() + " ";
+            //}
+
+            //Debug.Log(msg);
+        }
+    }
+}
